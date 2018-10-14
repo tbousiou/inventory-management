dce6cbca7e66ecf62ce6d62cfee95ef693fc530b
Theodoros Bousios <tbousiou@gmail.com>
2018-10-14 13:43:18 +0300

    base admin template prototype

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	user = {'username': 'Teo'}
+	assets = [
+		{
+			'model': 'Macbook Pro',
+			'manufacturer': 'Apple',
+			'location': 'Lab 15'
+		},
+		{
+			'model': 'MX 345',
+			'manufacturer': 'Dell',
+			'location': 'Lab 12'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, assets=assets)
