38bdaedbc249e59780b6d34f469287cb4dd4f795
Theodoros Bousios <tbousiou@gmail.com>
2018-10-20 19:55:55 +0300

    Added basic routes and view templates for all models

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-from app.models import Location
+from app.models import Asset, Location, Category, Manufacturer, Department, User
 
 @app.route('/')
 @app.route('/index')
@@ -32,7 +32,34 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
+
+@app.route('/assets')
+def assets():
+	assets = Asset.query.all()
+	return render_template('assets.html', title='Assets', assets=assets)
+
+
 @app.route('/locations')
-def show():
+def locations():
 	locations = Location.query.all()
 	return render_template('locations.html', title='Locations', locations=locations)
+
+@app.route('/departments')
+def departments():
+	departments = Department.query.all()
+	return render_template('departments.html', title='Departments', departments=departments)
+
+@app.route('/categories')
+def categories():
+	categories = Category.query.all()
+	return render_template('categories.html', title='Categories', categories=categories)
+
+@app.route('/manufacturers')
+def manufacturers():
+	manufacturers = Manufacturer.query.all()
+	return render_template('manufacturers.html', title='Manufacturers', manufacturers=manufacturers)
+
+@app.route('/users')
+def users():
+	users = User.query.all()
+	return render_template('users.html', title='Users', users=users)
