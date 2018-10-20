295d9ce18bf8346f97a5b4ecddfd83174b9c3f50
Theodoros Bousios <tbousiou@gmail.com>
2018-10-20 18:57:43 +0300

    Basic database models and relations

--- app/models.py
+++ app/models.py
@@ -14,16 +14,52 @@ class User(db.Model):
 class Asset(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	model = db.Column(db.String(64), index=True, unique=False)
-	manufacturer = db.Column(db.String(64), index=True, unique=True)
-	location = db.Column(db.String(64), index=True, unique=True)
+	
+	manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturer.id'))
+	location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+	category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
 	def __repr__(self):
 		return '<Asset {}>'.format(self.model)
 
 
+class Category(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+
+	assets = db.relationship('Asset', backref="category")
+
+	def __repr__(self):
+		return '<Category {}>'.format(self.name)
+
+
+class Manufacturer(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	
+	assets = db.relationship('Asset', backref="manufacturer")
+
+	def __repr__(self):
+		return '<Manufacturer {}>'.format(self.name)
+
+
 class Location(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), index=True, unique=True)
+	name = db.Column(db.String(64))
+
+	department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
+	
+	assets = db.relationship('Asset', backref="location")
 
 	def __repr__(self):
 		return '<Location {}>'.format(self.name)
+
+class Department(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+
+
+	locations = db.relationship('Location', backref="department")
+
+	def __repr__(self):
+		return '<Department {}>'.format(self.name)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import Location
 
 @app.route('/')
 @app.route('/index')
@@ -29,3 +30,9 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/locations')
+def show():
+	locations = Location.query.all()
+	return render_template('locations.html', title='Locations', locations=locations)
