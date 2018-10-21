aa8bfaf22f151792086113fbbe05ce357508bfac
Theodoros Bousios <tbousiou@gmail.com>
2018-10-21 21:25:37 +0300

    Unique department validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Department
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -16,3 +17,9 @@ class LocationForm(FlaskForm):
 
 class DepartmentForm(FlaskForm):
     name = StringField('Department Name', validators=[DataRequired()])
+
+
+    def validate_name(self, name):
+        department = Department.query.filter_by(name=name.data).first()
+        if department is not None:
+            raise ValidationError('Department alreadey exists. Please use another name.')
